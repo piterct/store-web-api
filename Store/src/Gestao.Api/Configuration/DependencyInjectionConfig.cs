7d037ac6ad213b263afa2759c31bb89ccebe8e8c
Michael Peter <michael_piterct@hotmail.com>
2022-01-20 02:27:10 -0300

    configure dependecy injection

---
+++ Store/src/Gestao.Api/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,19 @@
+﻿using Gestao.Business.Interfaces;
+using Gestao.Data.Context;
+using Gestao.Data.Repository;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Gestao.Api.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static IServiceCollection ResolveDependencies(this IServiceCollection services)
+        {
+            services.AddScoped<GestaoDbContext>();
+            services.AddScoped<IFornecedorRepository, FornecedorRepository>();
+            services.AddScoped<IEnderecoRepository, EnderecoRepository>();
+
+            return services;
+        }
+    }
+}
--- Store/src/Gestao.Api/Startup.cs
+++ Store/src/Gestao.Api/Startup.cs
@@ -1,7 +1,10 @@
+using Gestao.Api.Configuration;
+using Gestao.Data.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,9 +28,16 @@ namespace Gestao.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<GestaoDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
             services.AddAutoMapper(typeof(Startup));
 
             services.AddControllers();
+
+            services.ResolveDependencies();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
