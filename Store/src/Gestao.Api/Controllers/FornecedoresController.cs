b1baba870cba061579e1e1686b324b2f9e3b9f22
Michael Peter <michael_piterct@hotmail.com>
2022-01-20 01:40:50 -0300

    IFornecedorRepository

---
+++ Store/src/Gestao.Api/Controllers/FornecedoresController.cs
@@ -0,0 +1,26 @@
+﻿using Gestao.Api.ViewModels;
+using Gestao.Business.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Gestao.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class FornecedoresController : MainController
+    {
+        private readonly IFornecedorRepository _fornecedorRepository;
+
+        public FornecedoresController(IFornecedorRepository fornecedorRepository)
+        {
+            _fornecedorRepository = fornecedorRepository;
+        }
+
+        public async Task<ActionResult<IEnumerable<FornecedorViewModel>>> ObterTodos()
+        {
+            var fornecedores = await _fornecedorRepository.ObterTodos();
+
+            return Ok(fornecedores);
+        }
+    }
+}
