ad2c3cf80df68addb6f7bd94279ff4ae67145c09
Michael Peter <michael_piterct@hotmail.com>
2022-01-20 01:48:14 -0300

    AutoMapper config

---
+++ Store/src/Gestao.Api/Configuration/AutoMapperConfig.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Gestao.Api.ViewModels;
+using Gestao.Business.Models;
+
+namespace Gestao.Api.Configuration
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<Fornecedor, FornecedorViewModel>().ReverseMap();
+            CreateMap<Endereco, EnderecoViewModel>().ReverseMap();
+            CreateMap<Produto, ProdutoViewModel>().ReverseMap();
+        }
+    }
+}
