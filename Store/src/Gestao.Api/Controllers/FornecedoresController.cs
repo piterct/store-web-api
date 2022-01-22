7178bd339a208d803a368aceba74d8dbd3482142
Michael Peter <michael_piterct@hotmail.com>
2022-01-21 23:22:09 -0300

    ObterPorId

--- Store/src/Gestao.Api/Controllers/FornecedoresController.cs
+++ Store/src/Gestao.Api/Controllers/FornecedoresController.cs
@@ -2,6 +2,7 @@
 using Gestao.Api.ViewModels;
 using Gestao.Business.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -19,11 +20,26 @@ namespace Gestao.Api.Controllers
             _fornecedorRepository = fornecedorRepository;
             _mapper = mapper;
         }
-
+        [HttpGet]
         public async Task<IEnumerable<FornecedorViewModel>> ObterTodos()
         {
             var fornecedores = _mapper.Map<IEnumerable<FornecedorViewModel>>(await _fornecedorRepository.ObterTodos());
             return fornecedores;
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<FornecedorViewModel>> ObterPorId(Guid id)
+        {
+            var fornecedor = await ObterFornecedorProdutosEndereco(id);
+
+            if (fornecedor == null) return NotFound();
+
+            return fornecedor;
+        }
+
+        public async Task<FornecedorViewModel> ObterFornecedorProdutosEndereco(Guid id)
+        {
+            return _mapper.Map<FornecedorViewModel>(await _fornecedorRepository.ObterPorId(id));
         }
     }
 }
