1aeccc3957a71ceb893d9e289cd8bc391a668acf
Michael Peter <michael_piterct@hotmail.com>
2022-01-25 00:56:10 -0300

    ObterTodos

--- Store/src/Gestao.Api/Controllers/ProdutosController.cs
+++ Store/src/Gestao.Api/Controllers/ProdutosController.cs
@@ -1,15 +1,32 @@
 ﻿using AutoMapper;
+using Gestao.Api.ViewModels;
 using Gestao.Business.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Gestao.Api.Controllers
 {
+    [Route("api/produtos")]
     public class ProdutosController : MainController
     {
         private readonly IProdutoRepository _produtoRepository;
         private readonly IProdutoService _produtoService;
         private readonly IMapper _mapper;
-        public ProdutosController(INotificador notificador) : base(notificador)
+        public ProdutosController(INotificador notificador,
+                                  IProdutoRepository produtoRepository,
+                                  IProdutoService produtoService,
+                                  IMapper mapper) : base(notificador)
         {
+            _produtoRepository = produtoRepository;
+            _produtoService = produtoService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<ProdutoViewModel>> ObterTodos()
+        {
+            return _mapper.Map<IEnumerable<ProdutoViewModel>>(await _produtoRepository.ObterProdutosFornecedores());
         }
     }
 }
