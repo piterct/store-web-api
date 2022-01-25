0e1640f9310e8315cdd179c8351fc0f177df9a38
Michael Peter <michael_piterct@hotmail.com>
2022-01-25 00:33:20 -0300

    ProdutosController

---
+++ Store/src/Gestao.Api/Controllers/ProdutosController.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Gestao.Business.Interfaces;
+
+namespace Gestao.Api.Controllers
+{
+    public class ProdutosController : MainController
+    {
+        private readonly IProdutoRepository _produtoRepository;
+        private readonly IProdutoService _produtoService;
+        private readonly IMapper _mapper;
+        public ProdutosController(INotificador notificador) : base(notificador)
+        {
+        }
+    }
+}
