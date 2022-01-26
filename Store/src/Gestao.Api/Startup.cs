680a7202ee21b8ee3cc0e223c64d2d6f2eaba325
Michael Peter <michael_piterct@hotmail.com>
2022-01-26 01:43:31 -0300

    AddCors

--- Store/src/Gestao.Api/Startup.cs
+++ Store/src/Gestao.Api/Startup.cs
@@ -2,17 +2,11 @@ using Gestao.Api.Configuration;
 using Gestao.Data.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Gestao.Api
 {
@@ -44,6 +38,14 @@ namespace Gestao.Api
                 options.SuppressModelStateInvalidFilter = true;
             });
 
+            services.AddCors(options =>
+                options.AddPolicy("Development",
+                builder => builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+
+            ));
+
             services.ResolveDependencies();
         }
 
@@ -55,12 +57,14 @@ namespace Gestao.Api
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseCors("Development");
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
 
-            app.UseAuthorization();
-
+           // app.UseAuthorization();
+          
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
