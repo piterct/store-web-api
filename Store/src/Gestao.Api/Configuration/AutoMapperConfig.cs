78bddc823ac9b7e47047655702de0d18f5efafa2
Michael Peter <michael_piterct@hotmail.com>
2022-01-27 01:36:26 -0300

    automapper ProdutoImagemViewModel ReverseMap()

--- Store/src/Gestao.Api/Configuration/AutoMapperConfig.cs
+++ Store/src/Gestao.Api/Configuration/AutoMapperConfig.cs
@@ -12,6 +12,8 @@ namespace Gestao.Api.Configuration
             CreateMap<Endereco, EnderecoViewModel>().ReverseMap();
             CreateMap<ProdutoViewModel, Produto>();
 
+            CreateMap<ProdutoImagemViewModel, Produto>().ReverseMap();
+
             CreateMap<Produto, ProdutoViewModel>()
            .ForMember(dest => dest.NomeFornecedor, opt => opt.MapFrom(src => src.Fornecedor.Nome));
         }
