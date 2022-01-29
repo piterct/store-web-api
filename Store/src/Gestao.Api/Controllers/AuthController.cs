fa6817408e9f28fc92fe042883f5546987876dee
Michael Peter <michael_piterct@hotmail.com>
2022-01-29 19:50:04 -0300

    AuthController

---
+++ Store/src/Gestao.Api/Controllers/AuthController.cs
@@ -0,0 +1,11 @@
+﻿using Gestao.Business.Interfaces;
+
+namespace Gestao.Api.Controllers
+{
+    public class AuthController : MainController
+    {
+        public AuthController(INotificador notificador) : base(notificador)
+        {
+        }
+    }
+}
