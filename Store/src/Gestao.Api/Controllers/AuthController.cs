6fe39a458a1e117df16036675c03ed4e7778d18d
Michael Peter <michael_piterct@hotmail.com>
2022-01-29 20:12:11 -0300

    register user

--- Store/src/Gestao.Api/Controllers/AuthController.cs
+++ Store/src/Gestao.Api/Controllers/AuthController.cs
@@ -1,11 +1,49 @@
-﻿using Gestao.Business.Interfaces;
+﻿using Gestao.Api.ViewModels;
+using Gestao.Business.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace Gestao.Api.Controllers
 {
+    [Route("api")]
     public class AuthController : MainController
     {
-        public AuthController(INotificador notificador) : base(notificador)
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        public AuthController(INotificador notificador,
+                              SignInManager<IdentityUser> signInManager,
+                              UserManager<IdentityUser> userManager) : base(notificador)
         {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpPost("nova-conta")]
+        public async Task<ActionResult> Registrar(RegisterUserViewModel registerUser)
+        {
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+            var user = new IdentityUser
+            {
+                UserName = registerUser.Email,
+                Email = registerUser.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, registerUser.Password);
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+                return CustomResponse(registerUser);
+            }
+
+            foreach (var error in result.Errors)
+            {
+                NotificarErro(error.Description);
+            }
+
+            return CustomResponse();
         }
     }
 }
