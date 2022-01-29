7efbba537bf49d90b32c95093ab3aa75e69523fe
Michael Peter <michael_piterct@hotmail.com>
2022-01-29 09:45:45 -0300

    AddIdentityConfiguration

---
+++ Store/src/Gestao.Api/Configuration/IdentityConfig.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Gestao.Api.Configuration
+{
+    public static class IdentityConfig
+    {
+        public static IServiceCollection AddIdentityConfiguration(this IServiceCollection services)
+        {
+
+            return services;
+        }
+    }
+}
