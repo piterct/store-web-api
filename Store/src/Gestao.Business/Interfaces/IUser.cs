e64f32edd08e654108092ca8478b39a7b3d1efb9
Michael Peter <michael_piterct@hotmail.com>
2022-01-30 21:09:15 -0300

    IUser

---
+++ Store/src/Gestao.Business/Interfaces/IUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace Gestao.Business.Interfaces
+{
+    public interface IUser
+    {
+        string Name { get; }
+        Guid GetUserId();
+        string GetUserEmail();
+        bool IsAuthenticated();
+        bool IsInRole(string role);
+        IEnumerable<Claim> GetClaimsIdentity();
+
+    }
+}
