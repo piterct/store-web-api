b4101aed904e1847ca8fcbc2f522cccf8a7eff55
Michael Peter <michael_piterct@hotmail.com>
2022-02-02 19:58:32 -0300

    LoggerConfig

---
+++ Store/src/Gestao.Api/Configuration/LoggerConfig.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Gestao.Api.Configuration
+{
+    public static class LoggerConfig
+    {
+        public static IServiceCollection AddLoggingConfiguration(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static IApplicationBuilder UseLoggingConfiguration(this IApplicationBuilder app)
+        {
+            return app;
+        }
+    }
+}
