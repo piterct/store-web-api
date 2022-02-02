834ebc714cdb73237b3baa11cc5e4a2671d21984
Michael Peter <michael_piterct@hotmail.com>
2022-02-02 00:56:10 -0300

    types of logs

--- Store/src/Gestao.Api/V1/Controllers/AuthController.cs
+++ Store/src/Gestao.Api/V1/Controllers/AuthController.cs
@@ -4,6 +4,7 @@ using Gestao.Api.ViewModels;
 using Gestao.Business.Interfaces;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using System;
@@ -22,15 +23,17 @@ namespace Gestao.Api.V1.Controllers
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly UserManager<IdentityUser> _userManager;
         private readonly AppSettings _appSettings;
+        private readonly ILogger _logger;
         public AuthController(INotificador notificador,
                               SignInManager<IdentityUser> signInManager,
                               UserManager<IdentityUser> userManager,
                               IOptions<AppSettings> appSettings,
-                              IUser user) : base(notificador, user)
+                              IUser user, ILogger<AuthController> logger) : base(notificador, user)
         {
             _signInManager = signInManager;
             _userManager = userManager;
             _appSettings = appSettings.Value;
+            _logger = logger;
         }
 
         [HttpPost("nova-conta")]
@@ -69,6 +72,7 @@ namespace Gestao.Api.V1.Controllers
 
             if (result.Succeeded)
             {
+                _logger.LogInformation("Usuario " + loginUser.Email + " logado com sucesso");
                 return CustomResponse(await GerarJwt(loginUser.Email));
             }
             if (result.IsLockedOut)
