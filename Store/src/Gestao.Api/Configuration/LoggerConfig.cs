dd3ce7cae28add3d900e36135037734ca78d5e56
Michael Peter <michael_piterct@hotmail.com>
2022-02-03 00:25:00 -0300

    Configuration AddHealthChecks with db

--- Store/src/Gestao.Api/Configuration/LoggerConfig.cs
+++ Store/src/Gestao.Api/Configuration/LoggerConfig.cs
@@ -1,5 +1,9 @@
 ﻿using Elmah.Io.Extensions.Logging;
+using Gestao.Api.Extensions;
+using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System;
@@ -8,20 +12,50 @@ namespace Gestao.Api.Configuration
 {
     public static class LoggerConfig
     {
-        public static IServiceCollection AddLoggingConfiguration(this IServiceCollection services)
+        public static IServiceCollection AddLoggingConfiguration(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddElmahIo(o =>
-            {
-                o.ApiKey = "31cfbfe71ba9408994a821834aaab398";
-                o.LogId = new Guid("48225e5c-216c-4226-9121-53a1c75b950d");
-            });
+            //services.AddElmahIo(o =>
+            //{
+            //    o.ApiKey = "ApiKey";  Inclusao ApiKey DO ElmahIo
+            //    o.LogId = new Guid(); Inclusao LogId DO ElmahIo
+            //});
+
+            services.AddHealthChecks()
+               //.AddElmahIoPublisher(options =>
+               //{
+               //    options.ApiKey = "ApiKey";  Inclusao ApiKey DO ElmahIo
+               //    options.LogId = new Guid(); Inclusao LogId DO ElmahIo
+               //    options.HeartbeatId = "HeartbeatId";
+
+               //})
+               .AddCheck("Produtos", new SqlServerHealthCheck(configuration.GetConnectionString("DefaultConnection")))
+               .AddSqlServer(configuration.GetConnectionString("DefaultConnection"), name: "BancoSQL");
+
+            services.AddHealthChecksUI()
+                 .AddSqlServerStorage(configuration.GetConnectionString("DefaultConnection"));
 
             return services;
         }
 
         public static IApplicationBuilder UseLoggingConfiguration(this IApplicationBuilder app)
         {
-            app.UseElmahIo();
+            //app.UseElmahIo();
+
+            app.UseHealthChecks("/api/hc", new HealthCheckOptions
+            {
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+
+            app.UseHealthChecksUI(options =>
+            {
+                options.UIPath = "/api/hc-ui";
+                options.ResourcesPath = $"{options.UIPath}/resources";
+                options.UseRelativeApiPath = false;
+                options.UseRelativeResourcesPath = false;
+                options.UseRelativeWebhookPath = false;
+
+            });
 
             return app;
         }
