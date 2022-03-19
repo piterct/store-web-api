9cfc1529f476b7e312f408ccf5def114369210f1
Michael Peter <michael_piterct@hotmail.com>
2022-03-19 10:45:23 -0300

    CommandEntity

---
+++ Store/src/Gestao.Business/Commands/Shared/CommandEntity.cs
@@ -0,0 +1,10 @@
+﻿using Flunt.Notifications;
+using Gestao.Business.Commands.Interfaces;
+
+namespace Gestao.Business.Commands.Shared
+{
+    public abstract class CommandEntity : Notifiable, ICommand
+    {
+        public abstract void Validate();
+    }
+}
